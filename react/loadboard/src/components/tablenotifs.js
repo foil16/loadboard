017fb3aec660db8b78ead125a3b2b7d757b91cb5
foil16 <silverm4321@gmail.com>
2023-11-19 01:22:10 -0500

    --THIS WORKS2--

--- react/loadboard/src/components/tablenotifs.js
+++ react/loadboard/src/components/tablenotifs.js
@@ -1,25 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useParams } from "react-router-dom";
 
-const TableNotif = ({ truckId }) => {
-  const [notifications, setNotifications] = useState([]);
-
+const TableNotif = () => {
+  const [data, setData] = useState([]);
+  const navigate = useNavigate(); // Initialize navigate function
+  const { truckId } = useParams();
   useEffect(() => {
-    // WebSocket connection setup
     const socket = new WebSocket("ws://localhost:4001/");
 
     socket.onopen = () => {
       console.log("WebSocket connection established");
-      // Optionally, if your backend supports it, send a message to the server
-      // to subscribe only to notifications for the selected truckId
-      socket.send(JSON.stringify({ action: "subscribe", truckId: truckId }));
     };
 
     socket.onmessage = (event) => {
       try {
-        const message = JSON.parse(event.data);
-        // Filter out messages that are not notifications or don't match the truckId
-        if (message.type === "Notification" && message.truckId.toString() === truckId) {
-          setNotifications((prevNotifications) => [...prevNotifications, message]);
+        const messageData = JSON.parse(event.data);
+        console.log(messageData);
+        if (messageData.type === "Notification" || messageData.type === "End") {
+          //console.log("NOTIFICITATIONTNTNTIENIENEWOFIEWUOFWOUFGNOWGWJEGB");
+          console.log("Message received:", messageData);
+          if (messageData.type === "End") {
+            console.log("End of day detected");
+            setData([]); // Clear the data
+          } //if (messageData.truckerId.toString() === truckId.toString())
+          else {
+            //console.log("WE IN!ejfwegiuweoifhwioefjwoepfoewfioewhfowbeofnew");
+            setData((prevData) => [...prevData, messageData]);
+          }
         }
       } catch (error) {
         console.error("Error parsing message data:", error);
@@ -30,31 +38,52 @@ const TableNotif = ({ truckId }) => {
       console.error("WebSocket Error:", error);
     };
 
-    // Clean up the WebSocket connection when the component unmounts
+    // Clean up the socket when the component unmounts
     return () => {
+      console.log("Closing WebSocket connection");
       socket.close();
     };
-  }, [truckId]); // This effect should run when truckId changes
+  }, []);
+
+  const handleRowClick = (truckId) => {
+    navigate(`/truckers/${truckId}`); // Navigate to truckers route with truckId parameter
+  };
 
   return (
-    <div>
-      <h2>Notifications for Truck ID: {truckId}</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Truck ID</th>
-            <th>Load ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          {notifications.map((notification, index) => (
-            <tr key={index}>
-              <td>{notification.truckId}</td>
-              <td>{notification.loadId}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="carriers">
+      <body className="body-select-trucker">
+        <h2>Real-Time Truck Data</h2>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th className="px-6 py-3 text-left font-medium text-gray-500 uppercase tracking-wider">
+                  Truck ID
+                </th>
+                <th className="px-6 py-3 text-left font-medium text-gray-500 uppercase tracking-wider">
+                  Load ID
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200">
+              {data.map((item) => (
+                <tr
+                  key={item._id || item.truckId}
+                  className="hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleRowClick(item.truckId)}
+                >
+                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    {item.truckerId}
+                  </td>
+                  <td className="px-6 py-4 text-gray-500 whitespace-nowrap">
+                    {item.loadId}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </body>
     </div>
   );
 };
